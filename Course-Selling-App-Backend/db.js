bca9877c54675e7d188928b81d00397f6521a5ea
kunal <kunalbhalerao789@gmail.com>
2025-02-06 00:46:27 +0530

    step 4: Created user sign up endpoint

---
+++ Course-Selling-App-Backend/db.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+const ObjectId = Schema.ObjectId;
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const AdminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const CourseSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageUrl: String,
+  creatorId: ObjectId,
+});
+
+const PurchaseSchema = new Schema({
+  courseId: ObjectId,
+  userId: ObjectId,
+});
+
+const CourseContentSchema = new Schema({
+  videoUrl: String,
+  imageUrl: String,
+  price: String,
+});
+
+const UserModel = model("users", UserSchema);
+const AdminModel = model("admins", AdminSchema);
+const CourseModel = model("courses", CourseSchema);
+const PurchaseModel = model("purchases", PurchaseSchema);
+const CourseContentModel = model("courseContent", CourseContentSchema);
+
+module.exports = {
+  UserModel,
+  AdminModel,
+  CourseModel,
+  PurchaseModel,
+  CourseContentModel,
+};
--- Course-Selling-App-Backend/pages/user.js
+++ Course-Selling-App-Backend/pages/user.js
@@ -1,11 +1,62 @@
 const { Router } = require("express");
 const z = require("zod");
 const bcrypt = require("bcrypt");
+const { UserModel } = require("../db");
 
 const UserRouter = Router();
 
-UserRouter.get("/", (req, res) => {
-  res.send("User");
+UserRouter.post("/", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  const requireBody = z.object({
+    email: z.string().min(3).max(100).email(),
+    firstName: z.string().min(3).max(100),
+    lastName: z.string().min(3).max(100),
+    password: z
+      .string()
+      .min(3)
+      .max(100)
+      .refine((value) => /[A-Z]/.test(value), {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .refine((value) => /[a-z]/.test(value), {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .refine((value) => /\d/.test(value), {
+        message: "Password must contain at least one digit",
+      })
+      .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+        message: "Password must contain at least one special character",
+      }),
+  });
+
+  const parseUserDataWithSuccess = requireBody.safeParse(req.body);
+
+  if (!parseUserDataWithSuccess.success) {
+    res.status(403).json({
+      message: "Invalid email or password",
+      error: parseUserDataWithSuccess.error,
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  try {
+    const createUser = await UserModel.create({
+      email: email,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+    });
+    res.status(200).json({
+      message: "You are Signed Up 👏",
+      createUser,
+    });
+  } catch (e) {
+    es.status(403).json({
+      message: "User Alredy Exists !!!",
+    });
+  }
 });
 
 module.exports = {
